b847d14b8cedcb426b8ff24530a3b972f72f0acb
Richard <richardadebowale.oye@gmail.com>
2026-02-11 17:17:47 -0500

    config gets from env

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASE_URL = os.environ.get("DATABASE_URL")
 EMAIL = os.environ.get("EMAIL")
 EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-DEVBRAIN_API_KEY = os.environ.get("API_KEY")
+API_KEY = os.environ.get("DEVBRAIN_API_KEY")
 
 if not SECRET_KEY:
     raise RuntimeError("SECRET_KEY is not set")
