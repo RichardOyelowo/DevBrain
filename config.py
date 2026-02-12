562435e6b3394655a8ef0357afb135f22aed9c02
Richard <richardadebowale.oye@gmail.com>
2026-02-12 18:59:31 -0500

    config update

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 DATABASE_URL = os.environ.get("DATABASE_URL")
-EMAIL = os.environ.get("EMAIL")
-EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+MAIL_SERVER="smtp.mailgun.org"
+MAIL_USERNAME = os.environ.get("EMAIL")
+MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 API_KEY = os.environ.get("DEVBRAIN_API_KEY")
 
 if not SECRET_KEY:
