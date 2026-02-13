0f889000f5ed29d9eb01e62568700a29f14f06c6
Richard <richardadebowale.oye@gmail.com>
2026-02-12 20:50:57 -0500

    updated env var into class

--- app/config.py
+++ app/config.py
@@ -1,11 +1,15 @@
 import os
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
-DATABASE_URL = os.environ.get("DATABASE_URL")
-MAIL_SERVER="smtp.mailgun.org"
-MAIL_USERNAME = os.environ.get("EMAIL")
-MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-API_KEY = os.environ.get("DEVBRAIN_API_KEY")
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    DATABASE_URL = os.environ.get("DATABASE_URL")
 
-if not SECRET_KEY:
-    raise RuntimeError("SECRET_KEY is not set")
+    MAIL_SERVER = "smtp.mailgun.org"
+    MAIL_PORT = int(os.environ.get("MAIL_PORT", 587))
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("EMAIL")
+    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
+    SESSION_COOKIE_SECURE = True
