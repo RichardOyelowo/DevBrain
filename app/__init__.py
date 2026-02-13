18366692eab2587617db8f9a77b90be04f6b40a6
Richard <richardadebowale.oye@gmail.com>
2026-02-12 20:49:49 -0500

    deployment ready app configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,17 @@
-from flask import Flask, request, render_template, redirect, url_for, session
-from app.utils import calculate_grade, save_quiz_result
-from app.auth import login_required, get_db, auth
-from config import SECRET_KEY, DATABASE_URL
-from flask_wtf import CSRFProtect
-from question import Questions
-from app.routes import appp
-import os
-
-
+from flask import Flask
+from .config import Config
+from .extensions import mail, csrf
+from .routes import main
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    csrf = csrf.init_app(app)
+    mail.init_app(app)
+    csrf.init_app(app)
 
-    # Registering auth routes
-    app.register_blueprint(appp)
+    app.register_blueprint(main)
     app.register_blueprint(auth)
 
-    csrf.exempt(auth)
-
-    csrf = CSRFProtect(app)
-
-    app.secret_key = SECRET_KEY
-
-    # Session config
-    app.config.update(
-        SESSION_COOKIE_HTTPONLY=True,
-        SESSION_COOKIE_SAMESITE="Lax",
-        SESSION_COOKIE_SECURE=True,
-    )
-
     return app
-
