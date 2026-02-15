ad6f11b07010d63c06dff12ab036f6e2661fe7d2
Richard <richardadebowale.oye@gmail.com>
2026-02-14 21:12:21 -0500

    updated session to redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
+from flask_session import Session
 from flask import Flask
 from .config import Config
-from .extensions import mail, csrf
+from .extensions import mail, csrf, redis_client
 from .db import close_db
 from .routes import main
 from .auth import auth
@@ -23,4 +24,13 @@ def create_app():
     # helps with concurrency
     app.teardown_appcontext(close_db)
 
+    # ------------------ Redis Session ------------------
+    REDIS_URL = os.environ.get("REDIS_URL")
+    if REDIS_URL:
+        app.config['SESSION_TYPE'] = 'redis'
+        app.config['SESSION_REDIS'] = redis_client
+        app.config['SESSION_PERMANENT'] = False
+        app.config['SESSION_USE_SIGNER'] = True
+        Session(app)
+        
     return app
