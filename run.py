d71f0555fc8c9cc55f861d018c6824b000201f40
Richard <richardadebowale.oye@gmail.com>
2026-05-02 21:22:26 -0400

    full run setup

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
 from app import create_app
-from app.db import create_db
 
 app = create_app()
 
-with app.app_context():
-    create_db()
-
-
 if __name__ == "__main__":
+    from app.db import create_db
+
+    with app.app_context():
+        create_db()
     app.run(debug=True)
